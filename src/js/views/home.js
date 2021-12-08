2af70c61e3aae72a1a51c3e107c12ed04a14237e
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-12-08 16:51:21 +0000

    Add FullsNews component to tests

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,9 +2,20 @@ import React from "react";
 import "../../styles/home.scss";
 
 // Components
+import "../component/FullNews.jsx";
+import FullNews from "../component/FullNews.jsx";
 
 export const Home = () => (
-	<div className="text-center mt-5">
+	<div className="mt-5">
 		<h1>Tests</h1>
+		<h2>FullNews component</h2>
+		<FullNews
+			title="This is the title"
+			author="Author Name"
+			date="29/12/2021"
+			shortContent="The news hash.\nWith one or more paragraphs."
+			fullContent="It suposed to be larger than short content.\nThis is a second paragraph for the full content."
+			photoURL="https://picsum.photos/1024/800?random=1"
+		/>
 	</div>
 );
