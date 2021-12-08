0a12ff776b3fa365aad39f3c80c783c36ea87466
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-12-08 16:45:54 +0000

    FullNews component

--- src/js/component/FullNews.jsx
+++ src/js/component/FullNews.jsx
@@ -3,34 +3,32 @@ import PropTypes from "prop-types";
 
 // Resources
 
-import "../../styles/FullNews.scss"
+import "../../styles/FullNews.scss";
 
 // Component
 
-const FullNews = ({title, author, date, shortContent, fullContent, photoURL}) => {
-
-	function splitParagraph (string) {
-		return string.split("\n").map(
-			paragraph => {
-				<p>{paragraph}</p>
-			}
-		)
+const FullNews = ({ title, author, date, shortContent, fullContent, photoURL }) => {
+	function splitParagraphs(string) {
+		return string.split(/\\n+/gm).map((paragraph, idx) => <p key={idx}>{paragraph}</p>);
 	}
 
 	return (
 		<div className="container FullNews">
 			<div className="row">
-				<div className="col-12 col-lg-6">
-					<Link to={newsURL}><h2>{title}</h2></Link>
-					{splitParagraph(shortContent)}
-					<br/>
-					<p><span>{author}</span><span>{date}</span></p>
-					<img src={photoURL} alt={title}/>
-					{splitParagraph(fullContent)}
+				<div className="col-12">
+					<h2>{title}</h2>
+					{splitParagraphs(shortContent)}
+					<hr />
+					<p>
+						<span>{author}</span>
+						<span>{date}</span>
+					</p>
+					<img src={photoURL} alt={title} />
+					{splitParagraphs(fullContent)}
 				</div>
 			</div>
 		</div>
-	)
+	);
 };
 
 export default FullNews;
@@ -38,8 +36,8 @@ export default FullNews;
 FullNews.propTypes = {
 	title: PropTypes.string.isRequired,
 	author: PropTypes.string.isRequired,
-	date: PropTypes.number.isRequired,
+	date: PropTypes.string.isRequired,
 	shortContent: PropTypes.string.isRequired,
-	photoURL: PropTypes.string.isRequired,
-
+	fullContent: PropTypes.string.isRequired,
+	photoURL: PropTypes.string.isRequired
 };
