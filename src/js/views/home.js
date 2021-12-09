8910307559ba7cf73afecac5911a23cd7ff4522b
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-12-09 12:17:54 +0000

    HighLigthNews component test

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,6 +4,7 @@ import "../../styles/home.scss";
 // Components
 import "../component/FullNews.jsx";
 import FullNews from "../component/FullNews.jsx";
+import HighLigthNews from "../component/HighLigthNews.jsx";
 
 export const Home = () => (
 	<div className="mt-5">
@@ -15,6 +16,14 @@ export const Home = () => (
 			date="29/12/2021"
 			shortContent="The news hash.\nWith one or more paragraphs."
 			fullContent="It suposed to be larger than short content.\nThis is a second paragraph for the full content."
+			photoURL="https://picsum.photos/1024/800?random=1"
+		/>
+		<h2>HighLigthNews component</h2>
+		<HighLigthNews
+			title="This is the title"
+			author="Author Name"
+			date="29/12/2021"
+			shortContent="The news hash.\nWith one or more paragraphs."
 			photoURL="https://picsum.photos/1024/800?random=1"
 		/>
 	</div>
