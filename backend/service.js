5b3f86a5a79730201eb30f38e5e83c53bde3b9da
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-12-10 20:32:06 +0000

    Clean up

--- backend/service.js
+++ backend/service.js
@@ -106,27 +106,6 @@ app.get("/news/?idx=:idx",(req,res)=>{
     res.send(newsJSON(news, req.params.idx));
 });
 
-app.post('/logout/',(req,res)=>{
-    console.log('Cerrando sesión.')
-    req.session.authenticated = false;
-    res.sendStatus(200);
-});
-
-app.get( '/private/',function( req, res ) {
-    /**
-     * Si el middleware de autenticación lo permite muestra el área privada.
-     */
-    const secret = [
-        'https://www.jesusda.com/docs/ebooks/ebook_memorias%20de%20un%20ingeniero.pdf',
-        '42',
-        'https://es.wikipedia.org/wiki/El_sentido_de_la_vida,_el_universo_y_todo_lo_dem%C3%A1s#El_n%C3%BAmero_42',
-        'https://es.wikipedia.org/wiki/Bastard_Operator_from_Hell',
-        'https://es.wikipedia.org/wiki/The_IT_Crowd',
-    ];
-    console.log('Dando respuestas a la vida.')
-    return res.send( JSON.stringify(secret) ) ;
-});
-
 app.listen( appPort , ()=>{
     console.log(`Listo!: http://localhost:${appPort}/`)
 }) ;
